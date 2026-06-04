7f5b6375036ab0b6f58c81cb82c3d5fa15a90a5d
Manish <stbymkt@gmail.com>
2026-06-04 09:13:58 +0530

    pushing entire module 1 Programs

---
+++ DataTypesAndVariables.java
@@ -0,0 +1,18 @@
+package module1;
+public class DataTypesAndVariables 
+{
+	public static void main(String[] args) 
+	{
+		byte a=100;//declaration and initilization
+		System.out.println(a);//utilization
+		short b=25000;//declaration and initilization
+		System.out.println(b);//utilization
+		int c=47852114;//declaration and initilization
+		System.out.println(c);//utilization
+		long d=78954565214l;//declaration and initilization
+		System.out.println(d);//utilization
+		
+		
+
+	}
+}
---
+++ IfBlock.java
@@ -0,0 +1,16 @@
+package module1;
+public class IfBlock 
+{
+	public static void main(String[] args) 
+	{
+		int age=50;
+		
+		if(age>=18)//true
+		{
+			System.out.println("You can vote");
+		}
+		
+		System.out.println("Control will be here");
+		
+	}
+}
